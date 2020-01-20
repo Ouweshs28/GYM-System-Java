4b3c3e85976124f6893059e987091199885abd37
Ouweshs28 <cst2550@localhost.localdomain>
2020-01-20 12:55:06 +0400

    working

--- ClientConsole.java
+++ ClientConsole.java
@@ -2,24 +2,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 public class ClientConsole {
-  public static void main(String[] args){
+  public static void main(String[] args) {
 
     String hostName = "localhost";
     int portNumber = 9000;
 
     try (Socket clientSocket = new Socket(hostName, portNumber);
         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        ObjectInputStream inobj = new ObjectInputStream(clientSocket.getInputStream());
         BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))) {
-          System.out.println("Connection to server successful");
+      System.out.println("Connection to server successful");
       String userInput;
       while ((userInput = stdIn.readLine()) != null) {
         out.println(userInput);
+        switch (userInput) {
+        case "LISTALL":
+          ArrayList <Booking> bookings=new ArrayList<>();
+          try {
+            Object object = inobj.readObject();
+            bookings =  (ArrayList<Booking>) object;
+           for(Booking i:bookings){
+             i.printBooking();
+           }
+        } catch (ClassNotFoundException e) {
+            System.out.println("The title list has not come from the server");
+            e.printStackTrace();
+        }
+          break;
+        }
       }
     } catch (UnknownHostException e) {
       System.err.println("Don't know about host " + hostName);
--- ServerRunnable.java
+++ ServerRunnable.java
@@ -2,59 +2,70 @@ import java.net.Socket;
 import java.io.*;
 import java.util.*;
 
-public class ServerRunnable implements Runnable{
+public class ServerRunnable implements Runnable {
 
     private Socket socket;
 
     public ServerRunnable(Socket sk) {
-        this.socket=sk;
+        this.socket = sk;
     }
 
-    public void run(){
-        try (
-        OutputStream outs= socket.getOutputStream();
-        InputStream ins= socket.getInputStream();
-        PrintWriter out= new PrintWriter(outs, true);
-        Scanner in = new Scanner(ins);
-        ObjectOutputStream outobj =new ObjectOutputStream (outs);
-        )
-       
+    public void run() {
+        try (OutputStream outs = socket.getOutputStream();
+                InputStream ins = socket.getInputStream();
+                PrintWriter out = new PrintWriter(outs, true);
+                Scanner in = new Scanner(ins);
+                ObjectOutputStream outobj = new ObjectOutputStream(outs);)
+
         {
             String userinput;
-            while(in.hasNextLine()){
+            while (in.hasNextLine()) {
                 System.out.println();
-                userinput=in.nextLine();
-                if (userinput.contains("ADD")){
+                userinput = in.nextLine();
+                if (userinput.contains("ADD")) {
                     System.out.println("ADD Operation: " + userinput + " from client: " + socket.toString());
                     out.println(userinput);
-                }if(userinput.contains("LISTALL")){
-                    ArrayList <Booking> bookings=Query.listAll();
-                    for(Booking i:bookings)
-                    i.printBooking();
+                }
+                if (userinput.contains("LISTALL")) {
+                    ArrayList<Booking> bookings = Query.listAll();
+                    try {
+                        outobj.writeObject(bookings);
+                        System.out.println("Written");
+                    } catch (IOException e) {
+                        System.out.println("IO Exception");
+                        e.printStackTrace();
+                    }
                     System.out.println("LIST ALL Operation: " + userinput + " from client: " + socket.toString());
                     out.println(userinput);
-                }if(userinput.contains("LISTPT")){
-                    System.out.println("LIST PERSONAL TRANIER Operation: " + userinput + " from client: " + socket.toString());
+                }
+                if (userinput.contains("LISTPT")) {
+                    System.out.println(
+                            "LIST PERSONAL TRANIER Operation: " + userinput + " from client: " + socket.toString());
                     out.println(userinput);
-                }if(userinput.contains("LISTCLIENT")){
+                }
+                if (userinput.contains("LISTCLIENT")) {
                     System.out.println("LIST Client Operation: " + userinput + " from client: " + socket.toString());
                     out.println(userinput);
-                }if(userinput.contains("LISTDAY")){
-                    System.out.println("LIST BOOKING DAYS Operation: " + userinput + " from client: " + socket.toString());
+                }
+                if (userinput.contains("LISTDAY")) {
+                    System.out.println(
+                            "LIST BOOKING DAYS Operation: " + userinput + " from client: " + socket.toString());
                     out.println(userinput);
-                }if(userinput.contains("UPDATE")){
+                }
+                if (userinput.contains("UPDATE")) {
                     System.out.println("UPDATE BOOKING Operation: " + userinput + " from client: " + socket.toString());
                     out.println(userinput);
-                }if(userinput.contains("DELETE")){
+                }
+                if (userinput.contains("DELETE")) {
                     System.out.println("DELETE BOOKING Operation: " + userinput + " from client: " + socket.toString());
                     out.println(userinput);
-                }else{
+                } else {
                     System.out.println("Invalid Operation: " + userinput + " from client: " + socket.toString());
                     out.println(userinput);
                 }
-                
+
             }
-            
+
         } catch (IOException e) {
             System.out.println("Exception caught when trying to listen on port");
             System.out.println(e.getMessage());
